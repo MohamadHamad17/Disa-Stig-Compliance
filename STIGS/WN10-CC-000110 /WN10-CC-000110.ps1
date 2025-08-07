f597e454e3fdc7713ac293fb17b69a199a412042
Mohamad Hamad <mohamadhamad17@hotmail.com>
2025-08-06 22:46:13 -0400

    Update WN10-CC-000110.ps1

--- STIGS/WN10-CC-000110 /WN10-CC-000110.ps1
+++ STIGS/WN10-CC-000110 /WN10-CC-000110.ps1
@@ -1,1 +1,23 @@
+# Ensure administrator privileges
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+    ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You must run this script as Administrator."
+    exit 1
+}
 
+$regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Printers"
+$valueName = "DisableHTTPPrinting"
+
+try {
+    # Create registry path if it doesn't exist
+    if (-not (Test-Path $regPath)) {
+        New-Item -Path $regPath -Force | Out-Null
+    }
+
+    # Set the value to disable HTTP printing
+    Set-ItemProperty -Path $regPath -Name $valueName -Value 1
+
+    Write-Output "HTTP printing has been disabled successfully. (DisableHTTPPrinting = 1)"
+} catch {
+    Write-Error "Failed to apply the setting: $_"
+}
