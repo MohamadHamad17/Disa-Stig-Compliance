5952c6aef9b9a174fc1304c241aaa4a0ffa283bf
Mohamad Hamad <mohamadhamad17@hotmail.com>
2025-08-06 23:08:59 -0400

    Update WN10-CC-000185.ps1

--- STIGS/WN10-CC-000185/WN10-CC-000185.ps1
+++ STIGS/WN10-CC-000185/WN10-CC-000185.ps1
@@ -1,1 +1,23 @@
+# Ensure administrator privileges
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+    ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Please run this script as Administrator."
+    exit 1
+}
 
+$regPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer"
+$valueName = "NoAutorun"
+
+try {
+    # Create the registry path if it doesn't exist
+    if (-not (Test-Path $regPath)) {
+        New-Item -Path $regPath -Force | Out-Null
+    }
+
+    # Set the registry value to disable autorun
+    Set-ItemProperty -Path $regPath -Name $valueName -Value 1
+
+    Write-Output "Autorun has been disabled successfully. (NoAutorun = 1)"
+} catch {
+    Write-Error "Failed to apply the setting: $_"
+}
