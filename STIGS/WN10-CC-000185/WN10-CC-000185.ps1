98cb04a45069192b67a4920e9c6b80b60ca0aa09
Mohamad Hamad <mohamadhamad17@hotmail.com>
2025-08-06 23:09:54 -0400

    Update WN10-CC-000185.ps1

--- STIGS/WN10-CC-000185/WN10-CC-000185.ps1
+++ STIGS/WN10-CC-000185/WN10-CC-000185.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures that autorun commands are disabled to prevent automatic execution from removable media.
+
+.NOTES
+    Author          : Mohamad Hamad
+    LinkedIn        : linkedin.com/in/mohamad-hamad-428a15318/
+    GitHub          : github.com/MohamadHamad17
+    Date Created    : 2025-08-06
+    Last Modified   : 2028-08-06
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000185
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> STIG-ID-WN10-CC-000185.ps1 
+#>
+
 # Ensure administrator privileges
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
     ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
