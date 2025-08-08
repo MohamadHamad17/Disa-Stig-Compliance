15382f9de6db53f5bbeb098667909d28d4f7a2c0
Mohamad Hamad <mohamadhamad17@hotmail.com>
2025-08-07 20:49:27 -0400

    Update WN10-SO-000005.ps1

--- STIGS/WN10-SO-000005/WN10-SO-000005.ps1
+++ STIGS/WN10-SO-000005/WN10-SO-000005.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+    This PowerShell script disables the built-in Administrator account to reduce unauthorized access risk.
+
+.NOTES
+    Author          : Mohamad Hamad
+    LinkedIn        : linkedin.com/in/mohamad-hamad-428a15318/
+    GitHub          : github.com/MohamadHamad17
+    Date Created    : 2025-08-07
+    Last Modified   : 2028-08-07
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-SO-000005
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> STIG-ID-WN10-SO-000005.ps1 
+#>
+
 # Ensure administrator privileges
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
 ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
