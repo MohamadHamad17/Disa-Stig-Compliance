c03a4df136d419d2874f4122767608cba4f5c855
Mohamad Hamad <mohamadhamad17@hotmail.com>
2025-08-07 20:20:11 -0400

    Update WN10-CC-000145.ps1

--- STIGS/WN10-CC-000145/WN10-CC-000145.ps1
+++ STIGS/WN10-CC-000145/WN10-CC-000145.ps1
@@ -1,3 +1,30 @@
+<#
+.SYNOPSIS
+    This PowerShell script ensures users are required to enter a password when resuming from sleep on battery.
+
+.NOTES
+    Author          : Mohamad Hamad
+    LinkedIn        : linkedin.com/in/mohamad-hamad-428a15318/
+    GitHub          : github.com/MohamadHamad17
+    Date Created    : 2025-08-06
+    Last Modified   : 2028-08-06
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000145
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> STIG-ID-WN10-CC-000145.ps1 
+#>
+
 # Check for admin privileges
 if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
 ).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
