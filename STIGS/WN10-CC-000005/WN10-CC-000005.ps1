50baa9429ccf5410396bf2114706f3de5f3895af
Mohamad Hamad <mohamadhamad17@hotmail.com>
2025-08-07 21:56:17 -0400

    Update WN10-CC-000005.ps1

--- STIGS/WN10-CC-000005/WN10-CC-000005.ps1
+++ STIGS/WN10-CC-000005/WN10-CC-000005.ps1
@@ -1,3 +1,31 @@
+<#
+.SYNOPSIS
+    This script disables camera access from the lock screen to prevent unauthorized use before login.
+
+.NOTES
+    Author          : Mohamad Hamad
+    LinkedIn        : linkedin.com/in/mohamad-hamad-428a15318/
+    GitHub          : github.com/MohamadHamad17
+    Date Created    : 2025-08-07
+    Last Modified   : 2028-08-07
+    Version         : 1.0
+    CVEs            : N/A
+    Plugin IDs      : N/A
+    STIG-ID         : WN10-CC-000005 
+
+.TESTED ON
+    Date(s) Tested  : 
+    Tested By       : 
+    Systems Tested  : 
+    PowerShell Ver. : 
+
+.USAGE
+    Put any usage instructions here.
+    Example syntax:
+    PS C:\> STIG-ID-WN10-CC-000005 .ps1 
+#>
+
+
 # Ensure the registry path exists
 $regPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Personalization"
 if (-not (Test-Path $regPath)) {
