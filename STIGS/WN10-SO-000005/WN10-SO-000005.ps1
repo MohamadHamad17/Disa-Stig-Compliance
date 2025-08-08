c71c0c71b5d498b8281b5e8150a92a6a429bc9ae
Mohamad Hamad <mohamadhamad17@hotmail.com>
2025-08-07 20:47:52 -0400

    Update WN10-SO-000005.ps1

--- STIGS/WN10-SO-000005/WN10-SO-000005.ps1
+++ STIGS/WN10-SO-000005/WN10-SO-000005.ps1
@@ -1,1 +1,15 @@
+# Ensure administrator privileges
+if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()
+).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "Please run this script as Administrator."
+    exit 1
+}
+
+try {
+    # Disable the built-in Administrator account
+    Disable-LocalUser -Name "Administrator"
+    Write-Output "The built-in Administrator account has been disabled."
+} catch {
+    Write-Error "Failed to disable the Administrator account: $_"
+}
 
